091fba51cd6be91e0cc2224cfafcb2a1ea3c65ac
Quan <jmy.kot@gmail.com>
2023-12-14 16:42:26 +1100

    fix height

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,10 +1,10 @@
-import './Components.scss'
+import '../Components.scss'
 
 export default function Tabs() {
   return (
     <div className='tab'>
       <div className='top-space' />
-      
+        
     </div>
   )
 }
