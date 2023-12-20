073ac4dfb42f652f78f286348878d00291a0b15a
GregorGT <schilling.gregor@gmail.com>
2023-12-20 07:13:58 +0100

    Fixing the look of the button (class change) and changing the div elements to textareas.

--- src/components/TabComponent/Value.tsx
+++ src/components/TabComponent/Value.tsx
@@ -2,11 +2,11 @@ const Value = () => {
   return (
     <div className="value">
       <div>Formula</div>
-      <div className="editor">Entry: Sum( Modification Date)</div>
+      <textarea className="editor"/>
       <div>Value</div>
-      <div className="editor">-</div>
+      <textarea className="editor"/>
       <div className="flex justify-between">
-        <button className="apply-single mt-3">Apply single</button>
+        <button className="apply-filtered mt-3">Apply single</button>
         <button className="apply-filtered mt-3">Apply filtered</button>
       </div>
     </div>
