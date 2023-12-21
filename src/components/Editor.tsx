b85434e2aa7e334e5a406e895bf8449d226fbf81
shreyansh <shethshreyansh777@gmail.com>
2023-12-21 23:54:43 +0530

    editor added

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,256 +1,229 @@
 import "./editor.scss";
 
 import { Color } from "@tiptap/extension-color";
-import ListItem from "@tiptap/extension-list-item";
-import TextStyle from "@tiptap/extension-text-style";
-import { EditorProvider, useCurrentEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import Highlight from "@tiptap/extension-highlight";
-import { Button } from "react-bootstrap";
 import FontFamily from "@tiptap/extension-font-family";
-
+import Highlight from "@tiptap/extension-highlight";
+import ListItem from "@tiptap/extension-list-item";
 import Table from "@tiptap/extension-table";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
+import TextStyle from "@tiptap/extension-text-style";
+import { EditorProvider, useCurrentEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import {
+  ColorPicker,
+  Divider,
+  Dropdown,
+  Button as IconButton,
+  Select,
+  Space,
+} from "antd";
+import { Button } from "react-bootstrap";
+import {
+  BoldOutlined,
+  ItalicOutlined,
+  UnderlineOutlined,
+  UndoOutlined,
+  RedoOutlined,
+  TableOutlined,
+  DownOutlined,
+} from "@ant-design/icons";
+const textStyle = [
+  {
+    value: "Paragraph",
+    label: "Paragraph",
+    isActive: (editor: any) => editor.isActive("paragraph"),
+    onclick: (editor: any) => editor.chain().focus().toggleParagraph().run(),
+  },
+  {
+    value: "heading 1",
+    label: "heading 1",
+    isActive: (editor: any) => editor.isActive("heading", { level: 1 }),
+    onclick: (editor: any) =>
+      editor.chain().focus().toggleHeading({ level: 1 }).run(),
+  },
+  {
+    value: "heading 2",
+    label: "heading 2",
+    isActive: (editor: any) => editor.isActive("heading", { level: 2 }),
+    onclick: (editor: any) =>
+      editor.chain().focus().toggleHeading({ level: 2 }).run(),
+  },
+  {
+    value: "heading 3",
+    label: "heading 3",
+    isActive: (editor: any) => editor.isActive("heading", { level: 3 }),
+    onclick: (editor: any) =>
+      editor.chain().focus().toggleHeading({ level: 3 }).run(),
+  },
+  {
+    value: "heading 4",
+    label: "heading 4",
+    isActive: (editor: any) => editor.isActive("heading", { level: 4 }),
+    onclick: (editor: any) =>
+      editor.chain().focus().toggleHeading({ level: 4 }).run(),
+  },
+  {
+    value: "heading 5",
+    label: "heading 5",
+    isActive: (editor: any) => editor.isActive("heading", { level: 5 }),
+    onclick: (editor: any) =>
+      editor.chain().focus().toggleHeading({ level: 5 }).run(),
+  },
+  {
+    value: "heading 6",
+    label: "heading 6",
+    isActive: (editor: any) => editor.isActive("heading", { level: 6 }),
+    onclick: (editor: any) =>
+      editor.chain().focus().toggleHeading({ level: 6 }).run(),
+  },
+];
 
-import React from "react";
+const tableAcions = [
+  {
+    label: "Add Row Below",
+    value: "Add Row Below",
+    onClick: (editor: any) => editor.chain().focus().addRowAfter().run(),
+  },
+  {
+    label: "Add Column After",
+    value: "Add Column After",
+    onClick: (editor: any) => editor.chain().focus().addColumnAfter().run(),
+  },
+  {
+    label: "Delete Row",
+    value: "Delete Row",
+    onClick: (editor: any) => editor.chain().focus().deleteRow().run(),
+  },
+  {
+    label: "Delete Column",
+    value: "Delete Column",
+    onClick: (editor: any) => editor.chain().focus().deleteColumn().run(),
+  },
+  {
+    label: "Delete Table",
+    value: "Delete Table",
+    onClick: (editor: any) => editor.chain().focus().deleteTable().run(),
+  },
+];
 
 const MenuBar = () => {
   const { editor } = useCurrentEditor();
-
   if (!editor) {
     return null;
   }
 
   return (
     <div
+      className="d-flex flex-col gap-2"
       style={{
         margin: "20px",
       }}
     >
-      <Button
-        onClick={() =>
-          editor
-            .chain()
-            .focus()
-            .setFontFamily("Comic Sans MS, Comic Sans")
-            .run()
-        }
-        className={
-          editor.isActive("textStyle", {
-            fontFamily: "Comic Sans MS, Comic Sans",
-          })
-            ? "is-active"
-            : ""
-        }
-      >
-        Comic Sans
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
-        className={editor.isActive("bold") ? "is-active" : ""}
-      >
-        bold
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={!editor.can().chain().focus().toggleItalic().run()}
-        className={editor.isActive("italic") ? "is-active" : ""}
-      >
-        italic
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        disabled={!editor.can().chain().focus().toggleStrike().run()}
-        className={editor.isActive("strike") ? "is-active" : ""}
-      >
-        strike
-      </Button>
-      <br />
-      <Button
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        disabled={!editor.can().chain().focus().toggleCode().run()}
-        className={editor.isActive("code") ? "is-active" : ""}
-      >
-        code
-      </Button>
-      <Button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
-        clear marks
-      </Button>
-      <Button onClick={() => editor.chain().focus().clearNodes().run()}>
-        clear nodes
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive("paragraph") ? "is-active" : ""}
-      >
-        paragraph
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
-      >
-        h1
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
-      >
-        h2
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
-      >
-        h3
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
-      >
-        h4
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}
-      >
-        h5
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}
-      >
-        h6
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive("bulletList") ? "is-active" : ""}
-      >
-        bullet list
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive("orderedList") ? "is-active" : ""}
-      >
-        ordered list
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive("codeBlock") ? "is-active" : ""}
-      >
-        code block
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive("blockquote") ? "is-active" : ""}
-      >
-        blockquote
-      </Button>
-      <Button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-        horizontal rule
-      </Button>
-      <Button onClick={() => editor.chain().focus().setHardBreak().run()}>
-        hard break
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().undo().run()}
-        disabled={!editor.can().chain().focus().undo().run()}
-      >
-        undo
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().redo().run()}
-        disabled={!editor.can().chain().focus().redo().run()}
-      >
-        redo
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().setColor("#958DF1").run()}
-        className={
-          editor.isActive("textStyle", { color: "#958DF1" }) ? "is-active" : ""
-        }
-      >
-        purple
-      </Button>
-      <Button
-        onClick={() =>
-          editor.chain().focus().toggleHighlight({ color: "#ffc078" }).run()
-        }
-        className={
-          editor.isActive("highlight", { color: "#ffc078" }) ? "is-active" : ""
-        }
-      >
-        orange highlight
-      </Button>
-      <br />
-      <Button
-        onClick={() =>
-          editor
-            .chain()
-            .focus()
-            .insertTable({ rows: 3, cols: 3, withHeaderRow: true })
-            .run()
-        }
-      >
-        insertTable
-      </Button>
-      <Button onClick={() => editor.chain().focus().addColumnBefore().run()}>
-        addColumnBefore
-      </Button>
-      <Button onClick={() => editor.chain().focus().addColumnAfter().run()}>
-        addColumnAfter
-      </Button>
-      <Button onClick={() => editor.chain().focus().deleteColumn().run()}>
-        deleteColumn
-      </Button>
-      <Button onClick={() => editor.chain().focus().addRowBefore().run()}>
-        addRowBefore
-      </Button>
-      <Button onClick={() => editor.chain().focus().addRowAfter().run()}>
-        addRowAfter
-      </Button>
-      <Button onClick={() => editor.chain().focus().deleteRow().run()}>
-        deleteRow
-      </Button>
-      <Button onClick={() => editor.chain().focus().deleteTable().run()}>
-        deleteTable
-      </Button>
-      <Button onClick={() => editor.chain().focus().mergeCells().run()}>
-        mergeCells
-      </Button>
-      <Button onClick={() => editor.chain().focus().splitCell().run()}>
-        splitCell
-      </Button>
-      <Button onClick={() => editor.chain().focus().toggleHeaderColumn().run()}>
-        toggleHeaderColumn
-      </Button>
-      <Button onClick={() => editor.chain().focus().toggleHeaderRow().run()}>
-        toggleHeaderRow
-      </Button>
-      <Button onClick={() => editor.chain().focus().toggleHeaderCell().run()}>
-        toggleHeaderCell
-      </Button>
-      <Button onClick={() => editor.chain().focus().mergeOrSplit().run()}>
-        mergeOrSplit
-      </Button>
-      <Button
-        onClick={() =>
-          editor.chain().focus().setCellAttribute("colspan", 2).run()
-        }
-      >
-        setCellAttribute
-      </Button>
-      <Button onClick={() => editor.chain().focus().fixTables().run()}>
-        fixTables
-      </Button>
-      <Button onClick={() => editor.chain().focus().goToNextCell().run()}>
-        goToNextCell
-      </Button>
-      <Button onClick={() => editor.chain().focus().goToPreviousCell().run()}>
-        goToPreviousCell
-      </Button>
+      <div className="d-flex gap-2">
+        <Select
+          onChange={(value) => {
+            const style = textStyle.find((item) => item.value === value);
+            if (style) {
+              console.log(style);
+              style.onclick(editor);
+            }
+          }}
+          value={textStyle.find((item) => item.isActive(editor))?.value}
+          options={textStyle}
+        />
+        <Divider type="vertical" />
+
+        <IconButton
+          type={editor.isActive("bold") ? "primary" : "default"}
+          shape="circle"
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editor.can().chain().focus().toggleBold().run()}
+          icon={<BoldOutlined size={14} />}
+        />
+
+        <IconButton
+          type={editor.isActive("italic") ? "primary" : "default"}
+          shape="circle"
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editor.can().chain().focus().toggleItalic().run()}
+          icon={<ItalicOutlined size={14} />}
+        />
+        <IconButton
+          type={editor.isActive("underline") ? "primary" : "default"}
+          shape="circle"
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={!editor.can().chain().focus().toggleStrike().run()}
+          icon={<UnderlineOutlined size={14} />}
+        />
+        <Divider type="vertical" />
+        <IconButton
+          icon={<UndoOutlined size={14} />}
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={!editor.can().chain().focus().undo().run()}
+        />
+        <IconButton
+          icon={<RedoOutlined size={14} />}
+          onClick={() => editor.chain().focus().redo().run()}
+          disabled={!editor.can().chain().focus().redo().run()}
+        />
+      </div>
+      <div className="d-flex gap-2">
+        <ColorPicker
+          onChange={(color) => {
+            editor.chain().focus().setColor(color.toHexString()).run();
+          }}
+          defaultValue={"#000"}
+          value={editor.getAttributes("textStyle").color || "#000"}
+          showText={() => <span>Text Color</span>}
+        />
+
+        <ColorPicker
+          onChange={(color) => {
+            editor
+              .chain()
+              .focus()
+              .toggleHighlight({ color: color.toHexString() })
+              .run();
+          }}
+          defaultValue={"#fff"}
+          value={editor.getAttributes("highlight").color || "#fff"}
+          showText={() => <span>Background Color</span>}
+        />
+      </div>
+
+      <div className="d-flex gap-2">
+        <IconButton
+          icon={<TableOutlined size={14} />}
+          onClick={() =>
+            editor
+              .chain()
+              .focus()
+              .insertTable({ rows: 3, cols: 3, withHeaderRow: false })
+              .run()
+          }
+        >
+          insertTable
+        </IconButton>
+        <Select
+          style={{ width: 300 }}
+          onChange={(value) => {
+            const action = tableAcions.find((item) => item.label === value);
+            if (action) {
+              action.onClick(editor);
+            }
+          }}
+          value={"Table Actions"}
+          defaultValue={"Table Actions"}
+          options={tableAcions}
+        >
+          <Space>
+            Table Actions
+            <DownOutlined />
+          </Space>
+        </Select>
+      </div>
     </div>
   );
 };
@@ -291,4 +264,3 @@ export default () => {
     />
   );
 };
-  
