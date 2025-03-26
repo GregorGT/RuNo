78bd52b30da5e43ff2950e78c87663e72ee8f012
bruno00026 <coutinhogoncalvesbruno@gmail.com>
2025-03-26 10:41:18 +0000

    feat: add database item on menu

--- src/components/Dropdowns.tsx
+++ src/components/Dropdowns.tsx
@@ -1,16 +1,13 @@
-import { writeTextFile, writeFile, create, BaseDirectory } from "@tauri-apps/plugin-fs";
+import { writeTextFile, BaseDirectory } from "@tauri-apps/plugin-fs";
 import { downloadDir } from "@tauri-apps/api/path";
 import type { MenuProps } from "antd";
 import { Dropdown, notification } from "antd";
 import { useAtom, useAtomValue } from "jotai/react";
 import { editorStateAtom } from "../state/editor";
 import { exportEditorFunction, loadEditorAtom } from "../state/load";
-import { info } from "@tauri-apps/plugin-log";
-import * as path from '@tauri-apps/api/path';
+import * as path from "@tauri-apps/api/path";
 import { selectedTableStore, tableAtom, tableStore } from "../state/table";
 import { getExcelColumnName } from "../helper";
-import { ask } from '@tauri-apps/plugin-dialog';
-import { confirm } from '@tauri-apps/plugin-dialog';
 
 import "./Components.scss";
 import {
@@ -22,7 +19,9 @@ import {
   sortingAtom,
   sortingFnAtom,
 } from "../state/formula";
+import ConnectionManagerDialog from "./DatabaseConnection/ConnectionManagerDialog";
 
+import { useState } from "react";
 export default function Dropdowns() {
   const tables = useAtomValue(tableAtom);
   const [editorState, setState] = useAtom(editorStateAtom);
@@ -33,6 +32,8 @@ export default function Dropdowns() {
   const [sortingFn, setSortingFn] = useAtom(sortingFnAtom);
   const [filterEnabled, setFilterEnabled] = useAtom(isFilterEnable);
   const [sortingEnabled, setSortingEnabled] = useAtom(isSortingEnable);
+  const [isConnectionDialogVisible, setConnectionDialogVisible] =
+    useState(false);
   const save_data = () => {
     const data = {
       editorData: getEditorValue.fn(),
@@ -79,23 +80,17 @@ export default function Dropdowns() {
     { key: 1, label: "Copy" },
     { key: 2, label: "Paste" },
     { key: 3, label: "Text formatting" },
+    {
+      key: 4,
+      label: "SQL Connections",
+      onClick: () => setConnectionDialogVisible(true),
+    },
   ];
   const infoItems: MenuProps["items"] = [
     { key: 1, label: "About" },
     { key: 2, label: "Donate" },
     { key: 3, label: "Contact" },
   ];
-
-  // autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"
-  const AutoCorrect: MenuProps["items"] = [
-    { key: 1, label: "Spellcheck" },
-    { key: 2, label: "Language [EN]" },
-  //  { key: 3, label: "Autocomplete" },
-  //  { key: 4, label: "Autocorrect" },
-  //  { key: 5, label: "Autocapitalize" },
-  ];
-
-
 
   const [api, contextHolder] = notification.useNotification();
   const showNotificaiton = (message: string) => {
@@ -113,8 +108,13 @@ export default function Dropdowns() {
           items: fileItems,
           onClick: async (e) => {
             if (e.key === "2") {
-              const mypath = path.join(`${await downloadDir()}`,`export-${new Date().getTime()}.json`);
-              await writeTextFile(await mypath, JSON.stringify(save_data()), { baseDir: BaseDirectory.AppConfig })
+              const mypath = path.join(
+                `${await downloadDir()}`,
+                `export-${new Date().getTime()}.json`
+              );
+              await writeTextFile(await mypath, JSON.stringify(save_data()), {
+                baseDir: BaseDirectory.AppConfig,
+              });
               showNotificaiton(`File Saved as ${await mypath}`);
             }
             if (e.key === "1") {
@@ -136,22 +136,22 @@ export default function Dropdowns() {
                     table.addEventListener("click", (e) => {
                       const cell = e.target?.closest("td, th");
                       if (!cell) return;
-                  
+
                       const row = cell.parentElement;
                       const rowIndex = row.rowIndex + 1;
                       const cellIndex = cell.cellIndex;
-                  
+
                       const columnLetter = getExcelColumnName(cellIndex + 1);
-                  
+
                       const excelRef = `${columnLetter}${rowIndex}`;
-                  
+
                       selectedTableStore.setState({
                         id: table.id,
-                        excelRef: excelRef
+                        excelRef: excelRef,
                       });
                     });
                   });
-                
+
                   showNotificaiton("File Loaded");
                 };
               };
@@ -169,53 +169,10 @@ export default function Dropdowns() {
       <Dropdown menu={{ items: infoItems }} placement="bottomLeft">
         <span>Info</span>
       </Dropdown>
-      <Dropdown menu={{ items: AutoCorrect, onClick: async (e) => {
-            
-            // autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"
-            /*const AutoCorrect: MenuProps["items"] = [
-              { key: 1, label: "Spellcheck" },
-              { key: 2, label: "Language [EN]" },
-              { key: 3, label: "Autocomplete" },
-              { key: 4, label: "Autocorrect" },
-              { key: 5, label: "Autocapitalize" },
-            ];*/
-
-              if (e.key === "1") {
-                const editor = document.getElementById("editor");
-
-                const confirmation = await confirm(
-                  'Activate spell checking?',
-                  { title: 'Spell checking', kind: 'info' }
-                );
-                
-                //console.log(confirmation);
-
-                if(confirmation == true)
-                {
-                  editor?.setAttribute("spellcheck", "true" );
-                  //this.innerHTML = "<span>&#10003; spellcheck</span>";
-                  //console.log(this);
-
-                }else
-                {
-                  editor?.setAttribute("spellcheck", "false" );
-                }
-
-              } else if(e.key === "3")
-              {
-                const editor = document.getElementById("editor");
-                if(editor?.getAttribute("autocomplete") == "off")
-                  editor?.setAttribute("autocomplete", "on" );
-                else
-                  editor?.setAttribute("autocomplete", "off" ); 
-                
-                  console.log(editor?.getAttribute("autocomplete"));
-              }
-            }
-            
-            }} placement="bottomLeft">
-        <span>Spell checking</span>
-      </Dropdown>      
+      <ConnectionManagerDialog
+        visible={isConnectionDialogVisible}
+        onClose={() => setConnectionDialogVisible(false)}
+      />
     </div>
   );
 }
